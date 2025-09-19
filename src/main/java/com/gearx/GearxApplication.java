c542e25b8822d9143c1616b7f8c51d572bcecad3
chez1s <toptrainghieml@gmail.com>
2025-09-20 00:56:17 +0700

    base(init): con cho Minh

---
+++ src/main/java/com/gearx/GearxApplication.java
@@ -0,0 +1,12 @@
+package com.gearx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gearx/GearxApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gearx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearxApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}

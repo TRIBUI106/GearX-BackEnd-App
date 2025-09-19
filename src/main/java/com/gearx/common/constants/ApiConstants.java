aaf432c2489d7ce0a8009f65549cac3664ba86d1
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-09-20 01:15:15 +0700

    init(endpoint): added ApiConstants to unify endpoint

---
+++ src/main/java/com/gearx/common/constants/ApiConstants.java
@@ -0,0 +1,21 @@
+package com.gearx.common.constants;
+
+public class ApiConstants {
+    private ApiConstants() {}
+
+    public static final String BASE_API = "/api";
+    public static final String VERSION_V1 = BASE_API + "/v1";
+
+    public static final class Auth {
+        public static final String BASE = VERSION_V1 + "/auth";
+        public static final String REGISTER = "/register";
+        public static final String LOGIN = "/login";
+        public static final String LOGOUT = "/logout";
+        public static final String INTROSPECT = "/introspect";
+        public static final String FORGOT_REQUEST = "/forgot/request";
+        public static final String FORGOT_RESET = "/forgot/reset";
+
+        private Auth() {}
+    }
+
+}

741342fd3ce474e8d28589d39eb5c4d9b5979730
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-09-22 22:20:27 +0700

    chore(product): init product work flow

---
+++ src/main/java/com/gearx/feature/product/entity/Product.java
@@ -0,0 +1,38 @@
+package com.gearx.feature.product.entity;
+
+import java.math.BigDecimal;
+
+import com.gearx.common.model.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product extends BaseEntity {
+
+    Integer productId;
+
+    String name;
+    String shortDescription;
+    String description;
+    Integer brandId;
+    Integer categoryId;
+    String model;
+    // jsonb
+    String attributes;
+    BigDecimal price;
+    BigDecimal compareAtPrice;
+    String currencyCode; // character(10)
+    Integer stockQuantity;
+    Integer warrantyMonths; // smallint
+    String mainImageUrl;
+    String imageUrls;
+}

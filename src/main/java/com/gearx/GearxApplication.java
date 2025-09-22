fdb8fc5c41713243cf61707e13a9e5bb2ffbd9e1
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-09-22 22:21:43 +0700

    update(application): added timezone property

--- src/main/java/com/gearx/GearxApplication.java
+++ src/main/java/com/gearx/GearxApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GearxApplication {
 
     public static void main(String[] args) {
+
+        // Anti failed JDBC Connection
+        System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
+
         SpringApplication.run(GearxApplication.class, args);
     }
 }
